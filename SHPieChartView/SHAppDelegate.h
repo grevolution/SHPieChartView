3e46ac9f03f2492b9d844bbf6a87a69861ccdc3d
Shan Ul Haq <g@grevolution.me>
2014-03-01 14:25:08 +0800

    Initial Commit

---
+++ SHPieChartView/SHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SHAppDelegate.h
+//  SHPieChartView
+//
+//  Created by SHAN UL HAQ on 1/3/14.
+//  Copyright (c) 2014 grevolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
